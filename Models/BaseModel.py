f2864bc3de3e70b060ab145499e469edd1349a15
mxh <mxh403@163.com>
2020-05-05 10:16:32 +0800

    :card_file_box: 添加 MySQL 数据库连接

--- Models/BaseModel.py
+++ Models/BaseModel.py
@@ -1,7 +1,10 @@
 from peewee import *
+from playhouse.pool import PooledMySQLDatabase
 
 
-database = SqliteDatabase('account.db', pragmas={'journal_mode': 'wal', 'cache_size': -1024 * 64})
+database = PooledMySQLDatabase('account', max_connections=8, stale_timeout=300, host='127.0.0.1', user='root',
+                                 passwd='', charset='utf8', port=3306)
+# database = SqliteDatabase('account.db', pragmas={'journal_mode': 'wal', 'cache_size': -1024 * 64})
 
 
 class UnknownField(object):
